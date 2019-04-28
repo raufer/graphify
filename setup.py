1efb7ae88ecc9e7af12bbcee65f4b53d99d01921
raul ferreira <raufer92@gmail.com>
2019-04-28 22:24:33 +0100

    Extended setup.oy

--- setup.py
+++ setup.py
@@ -2,6 +2,11 @@ from setuptools import setup, find_packages
 
 setup(
     name='graphify',
+    version='1.0.0',
+    description='A library to help create graph structures out of flat ones',
+    author='raul ferreira',
+    author_email='raufer92@gmail.com',
+    license='MIT',
     packages=find_packages(),
     include_package_data=True,
     install_requires=[],
