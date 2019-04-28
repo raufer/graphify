3f14394f04d51f6751b5de56a0cc3d6ed1a6482b
raul ferreira <raufer92@gmail.com>
2019-04-28 22:21:47 +0100

    Added setup.py

--- setup.py
+++ setup.py
@@ -1,10 +1,8 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='lightapp',
+    name='graphify',
     packages=find_packages(),
     include_package_data=True,
-    install_requires=[
-        'flask',
-    ],
+    install_requires=[],
 )
